48b253f304e788db97007d3d1b89a13de48567ce
mariaisabel43 <mariaisabelarrieta24@gmail.com>
2023-04-12 22:53:10 -0600

    Destinos turisticos

---
+++ Controllers/DestinosController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Destinos.Models;
+using Destinos.Models.ViewsModels;
+
+namespace Destinos.Controllers
+{
+    public class DestinosController : Controller
+    {
+        // GET: Destinos
+        public ActionResult Index()
+        {
+            List<TablaViewModel> lst;
+            using (DestinosEntities db = new DestinosEntities())
+            {
+                lst = (from d in db.Destinos
+                       select new TablaViewModel
+                       {
+                           Name = d.Name,
+                           Photo = d.Photo,
+                           Description = d.Description,
+                           Price = (int)d.Price
+                       }).ToList();
+            }
+            return View(lst);
+        }
+    }
+}
---
+++ Models/ViewsModels/TablaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Destinos.Models.ViewsModels
+{
+    public class TablaViewModel
+    {
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        public string Description { get; set; } 
+        public int Price { get; set; }
+        
+    }
+}
